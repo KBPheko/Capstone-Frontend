0475cbb1ad18915b14fa1b7bfbc33524ab155dd5
Karabo Pheko <phekorefi@gmail.com>
2022-10-13 21:52:00 +0200

    created signin, createProfile, admin and user dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './footer/footer.component';
+import { SigninComponent } from './signin/signin.component';
+import { CreateprofileComponent } from './createprofile/createprofile.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    FooterComponent
+    FooterComponent,
+    SigninComponent,
+    CreateprofileComponent,
+    AdminDashboardComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
